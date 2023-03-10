7571361108abb3133ae3d5dd5ca6c7ce24cdf576
BloodSample <>
2023-03-10 20:27:25 +0400

    horizontal and vertical camera controls done

--- Shrinking gun/Assets/Scripts/PlayerMovementController.cs
+++ Shrinking gun/Assets/Scripts/PlayerMovementController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform pivotPoint;
+    [SerializeField] float mouseSensetivity = 1f;
+    float verticalRotStore;
+    Vector2 mouseInput;
+    private void Update()
     {
-        
+        GetVerticalRotation();
+        GetHorizontlRotation();
     }
-
-    // Update is called once per frame
-    void Update()
+    void GetHorizontlRotation()
     {
-        
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensetivity;
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,
+                             transform.rotation.eulerAngles.y + mouseInput.x, 
+                             transform.rotation.eulerAngles.z);
+    }
+    void GetVerticalRotation()
+    {
+        verticalRotStore += mouseInput.y;
+        verticalRotStore = Mathf.Clamp(verticalRotStore, -60, 60);
+
+        pivotPoint.rotation = Quaternion.Euler(- verticalRotStore, 
+                              pivotPoint.rotation.eulerAngles.y, 
+                              pivotPoint.rotation.eulerAngles.z);
     }
 }
