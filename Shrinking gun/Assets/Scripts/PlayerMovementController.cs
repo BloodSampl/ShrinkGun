d6a6499ec3961703fd37bd773f8475dc68b992ed
BloodSample <>
2023-03-11 08:08:59 +0400

    jumping

--- Shrinking gun/Assets/Scripts/PlayerMovementController.cs
+++ Shrinking gun/Assets/Scripts/PlayerMovementController.cs
@@ -6,12 +6,30 @@ public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField] Transform pivotPoint;
     [SerializeField] float mouseSensetivity = 1f;
-    float verticalRotStore;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float jumpForce;
+    Rigidbody rb;
     Vector2 mouseInput;
+    Vector3 moveInput;
+    Vector3 moveDir;
+
+    float verticalRotStore;
+    bool isGrounded;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
     private void Update()
     {
         GetVerticalRotation();
         GetHorizontlRotation();
+        PlayerMove();
+        PlayerJump();
     }
     void GetHorizontlRotation()
     {
@@ -28,5 +46,18 @@ public class PlayerMovementController : MonoBehaviour
         pivotPoint.rotation = Quaternion.Euler(- verticalRotStore, 
                               pivotPoint.rotation.eulerAngles.y, 
                               pivotPoint.rotation.eulerAngles.z);
+    }
+    void PlayerMove()
+    {
+        moveInput = new Vector3(Input.GetAxis("Horizontal"), rb.velocity.y, Input.GetAxis("Vertical")) * moveSpeed;
+        moveDir = (transform.forward * moveInput.z) + (transform.up * rb.velocity.y) + (transform.right * moveInput.x);
+        rb.velocity = moveDir;
+    }
+    void PlayerJump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y + jumpForce, rb.velocity.z);
+        }
     }
 }
