69ed11e28dc3624724ad11c814a86b3b04f93aaa
BloodSample <>
2023-03-12 11:20:53 +0400

    made a scale limit for the objects bieng scaled

--- Shrinking gun/Assets/Scripts/ScallingObjects.cs
+++ Shrinking gun/Assets/Scripts/ScallingObjects.cs
@@ -6,11 +6,14 @@ public class ScallingObjects : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
+        transform.localScale = new Vector3(Mathf.Clamp(transform.localScale.x, 0.15f, 5f),
+                                           Mathf.Clamp(transform.localScale.y, 0.15f, 5f),
+                                           Mathf.Clamp(transform.localScale.z, 0.15f, 5f));
         if (collision.gameObject.CompareTag("Big"))
         {
             transform.localScale += Vector3.one;
         }
-        else if (collision.gameObject.CompareTag("Small"))
+        else if (collision.gameObject.CompareTag("Small") && transform.localScale.x > 0.15f)
         {
             transform.localScale -= Vector3.one;
         }
