e697072c496e1f74bbf34b2e524f30114fc01399
BloodSample <>
2023-03-12 15:38:11 +0400

    Drop panel done

---
+++ Shrinking gun/Assets/Scripts/DropPoint.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropPoint : MonoBehaviour
+{
+    [SerializeField] string objectTag;
+    [SerializeField] Transform holdPoint;
+    [SerializeField] GameObject path;
+
+
+    private void Update()
+    {
+        if(holdPoint.childCount < 1)
+        {
+            path.SetActive(false);
+        }
+        else
+        {
+            path.SetActive(true);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag(objectTag))
+        {
+            GameObject obj = collision.gameObject;
+            obj.transform.position = holdPoint.position;
+            obj.transform.parent = holdPoint;
+            path.SetActive(true);
+        }
+    }
+}
